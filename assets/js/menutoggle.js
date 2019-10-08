f5ce89cb76c97e461504077b9c09b5893828d645
Laura <lauras_here@pm.me>
2019-10-07 21:03:10 -0700

    Retool page images and colors, fix UI issues, edit content

--- assets/js/menutoggle.js
+++ assets/js/menutoggle.js
@@ -1,16 +1,27 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-window.onscroll = function() {
-  const navbar = document.querySelector('#navbar');
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    navbar.classList.replace('bg-transparent', 'bg-primary');
-  } else if (document.body.scrollTop < 100 || document.documentElement.scrollTop < 100) {
-    navbar.classList.replace('bg-primary', 'bg-transparent');
-  }
-};
+const navbarBrand = document.querySelector('.navbar-brand');
+const navbarContent = document.querySelector('#navbarSupportedContent')
+const navbar = document.querySelector('#navbar');
 const menuToggler = document.querySelector('.navbar-toggler');
 const siteTitle = document.querySelector('.site-title');
+
+window.onscroll = function() {
+  this.console.log(navbarBrand.classList);
+  this.console.log(navbarContent.classList);
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    navbarBrand.classList.replace('opacity-90', 'opacity-0');
+    navbarContent.classList.replace('opacity-90', 'opacity-0');
+  } else if (document.body.scrollTop < 100 || document.documentElement.scrollTop < 100) {
+    navbarBrand.classList.replace('opacity-0', 'opacity-90');
+    navbarContent.classList.replace('opacity-0', 'opacity-90');
+  }
+};
+
 menuToggler.addEventListener('click', () => {
     siteTitle.classList.toggle('opacity-0');
+    navbar.classList.toggle('bg-primary');
+    navbarBrand.classList.toggle('opacity-0');
+    navbarContent.classList.toggle('opacity-0');
 });
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -37,7 +48,7 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" current", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "flex";
   dots[slideIndex-1].className += " current";
 }
 
